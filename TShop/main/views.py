5cd3f06f1a9ccb8d8d51091a57011a1652a59f22
maydayDM <Dimka.popenko@gmail.com>
2022-08-11 20:46:49 +0300

    New app created, html template added

---
+++ TShop/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/main.html')
+
+def categories(request):
+    return render(request, 'main/categories.html')
